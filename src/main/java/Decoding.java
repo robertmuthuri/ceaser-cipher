e5e6dcd139dd4eb33514acb70d42e8c24b00728c
Robert Muthuri <muthuri.r@gmail.com>
2019-08-31 13:47:44 +0300

    Adds parallel functional logic to the Decoding class.

--- src/main/java/Decoding.java
+++ src/main/java/Decoding.java
@@ -1,3 +1,35 @@
 public class Decoding {
+    private String plainText;
+    private int key;
+    private String cipheredText;
 
+    public  String decryptText(String inputText, int shiftKey) {
+        plainText = inputText;
+        cipheredText = "";
+        key = shiftKey;
+        for (int i = 0; i < plainText.length(); i++) {
+            char c = plainText.charAt(i);
+            if(Character.isLetter(c)) {
+                if(Character.isLowerCase(c)) {
+                    char shiftC = (char)( c - key);
+                    if(shiftC < 'a') {
+                        cipheredText += (char)(shiftC + (26 - shiftC));
+                    } else {
+                        cipheredText += shiftC;
+                    }
+                } else if (Character.isUpperCase(c)) {
+                    char shiftC = (char)( c - key);
+                    if(shiftC < 'A') {
+                        cipheredText += (char)(shiftC + (26 - shiftC));
+                    } else {
+                        cipheredText += shiftC;
+                    }
+                }
+
+            } else {
+                cipheredText += c;
+            }
+        }
+        return cipheredText;
+    }
 }
