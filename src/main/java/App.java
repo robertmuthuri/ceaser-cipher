1f860ba3ba093dc9e1ab01ce24f36835220233f7
Robert Muthuri <muthuri.r@gmail.com>
2019-09-02 08:17:58 +0300

    Add a global gitignore file to the home directory.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,20 +1,31 @@
 import java.io.Console;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class App {
     public static void main(String[]args){
+        String inputText = " ";
         Console myConsole = System.console();
+        BufferedReader bufferedReader  = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("What be you message oh mighty ceaser?");
-        String inputText = myConsole.readLine();
-        System.out.println("What be-est they key oh majesty?");
-        int inputKey = Integer.parseInt(myConsole.readLine());
-        Encoding input = new Encoding();
-        String cipherText = input.encryptText(inputText, inputKey);
-        System.out.println("Here is your encrypted message oh your majesty: " + cipherText);
+         try{
+             inputText = bufferedReader.readLine();
+             System.out.println("What be-est they key oh majesty?");
+             int inputKey = Integer.parseInt(bufferedReader.readLine());
+             Encoding input = new Encoding();
+             String cipherText = input.encryptText(inputText, inputKey);
+             System.out.println("Here is your encrypted message oh your majesty: " + cipherText);
 
-        Decoding output = new Decoding();
-        System.out.println("General, would you like to decrypt his majesty's message? If so enter appropriate key");
-        int decryptKey = Integer.parseInt(myConsole.readLine());
-        String decryptText = output.decryptText(cipherText, decryptKey);
-        System.out.println("General, his majesty's message is: " + decryptText);
+             Decoding output = new Decoding();
+             System.out.println("General, would you like to decrypt his majesty's message? If so enter appropriate key");
+             int decryptKey = Integer.parseInt(bufferedReader.readLine());
+             String decryptText = output.decryptText(cipherText, decryptKey);
+             System.out.println("General, his majesty's message is: " + decryptText);
+         }
+         catch(IOException e) {
+             e.printStackTrace();
+        }
     }
 }
